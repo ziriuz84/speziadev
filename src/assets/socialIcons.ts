267f1dc3c0f9a8281b2a1ea4e6ab4f323eeaff35
Sirio Negri <ziriuz84@gmail.com>
2023-04-29 12:53:35 +0200

    Aggiunte social Icons

---
+++ src/assets/socialIcons.ts
@@ -0,0 +1,27 @@
+import type { SocialIcons } from "../types";
+
+const socialIcons: SocialIcons = {
+  Github: `<i class="fa-brands fa-github"></i>`,
+  Facebook: `<i class="fa-brands fa-facebook-f"></i>`,
+  Instagram: `<i class="fa-brands fa-instagram"></i>`,
+  LinkedIn: `<i class="fa-brands fa-linkedin-in"></i>`,
+  Mail: `<i class="fa-solid fa-envelope"></i>`,
+  Twitter: `<i class="fa-brands fa-twitter"></i>`,
+  Twitch: `<i class="fa-brands fa-twitch"></i>`,
+  YouTube: `<i class="fa-brands fa-youtube"></i>`,
+  WhatsApp: `<i class="fa-brands fa-youtube"></i>`,
+  Snapchat: `<i class="fa-brands fa-youtube"></i>`,
+  Pinterest: `<i class="fa-brands fa-pinterest-p"></i>`,
+  TikTok: `<i class="fa-brands fa-tiktok"></i>`,
+  CodePen: `<i class="fa-brands fa-codepen"></i>`,
+  Discord: `<i class="fa-brands fa-discord"></i>`,
+  GitLab: `<i class="fa-brands fa-discord"></i>`,
+  Reddit: `<i class="fa-brands fa-reddit-alien"></i>`,
+  Skype: `<i class="fa-brands fa-skype"></i>`,
+  Steam: `<i class="fa-brands fa-steam"></i>`,
+  Telegram: `<i class="fa-brands fa-telegram"></i>`,
+  Mastodon: `<i class="fa-brands fa-mastodon"></i>`,
+};
+
+export default socialIcons;
+
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import type {SocialObjects} from "./types";
+
+export const SOCIALS: SocialObjects=[
+    {
+        name:"Github",
+        href: "https://github.com/ziriuz84/speziadev",
+        active: true,
+        linkTitle: "Github Repo",
+    }
+
+]
